3ee609941e959a0819298aabf963f7ae83c11769
Johan Björk <johanimon@gmail.com>
2016-06-07 03:40:12 +0200

    Add types to test component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -4,7 +4,7 @@ import {fetch, Endpoint} from '../actions/api'
 import {selectPerson} from '../actions/visualization'
 import * as d3 from 'd3'
 import * as _ from 'lodash'
-import {Person, partyColor} from '../types/person'
+import {Person, PartyColor} from '../types/person'
 import PersonPanel from './PersonPanel'
 import {AppState} from '../reducers/common'
 
@@ -15,89 +15,82 @@ interface ITestProps {
 }
 
 class Test extends React.Component<ITestProps, void>{
-    
-    svg: any = undefined
+
+    svg: d3.Selection<Person>
     w: number = 1000
     h: number = 1000
-    circles: any = undefined
-    
-    componentWillMount(){
-        console.log("lol")
-        const {dispatch} = this.props
+    circles: d3.Selection<Person>
+    svgRef = 'my-svg'
+
+    componentWillMount() {
+        console.log('lol')
+        const dispatch = this.props.dispatch
         dispatch(fetch(Endpoint.Person))
     }
-    
-    componentDidMount(){
-       
-       this.svg = d3.select(document.getElementById("d3root")).append("svg")
-            .attr("width", this.w)
-            .attr("height", this.h)
+
+    componentDidMount() {
+        this.svg = d3.select(this.refs[this.svgRef] as HTMLElement) as d3.Selection<Person>
     }
-    
-    circlePosition(circles: any): any {
-        console.log(circles)
-        if(circles === undefined) return 
-        const origin = {x:500, y:500}
+
+    circlePosition(circles: d3.Selection<Person>): void {
+        const origin = { x: 500, y: 500 }
         const r = 400
-        const theta = 2*Math.PI
+        const theta = 2 * Math.PI
         const member = 348
-        return circles
-            .transition(2000)
-            .ease("linear")
-            .attr("cx", (p: Person, i: number) => {return origin.x+r*Math.cos(theta*(member-i)/member)})
-            .attr("cy", (p: Person, i: number) => {return origin.y+r*Math.sin(theta*(member-i)/member)})
-            .attr("r", 3)
+
+        circles
+            .transition(2000 + '')
+            .ease('linear')
+            .attr('cx', (p: Person, i: number) => origin.x + r * Math.cos(theta * (member - i) / member))
+            .attr('cy', (p: Person, i: number) => origin.y + r * Math.sin(theta * (member - i) / member))
+            .attr('r', 3)
     }
-        
-    basePosition(circles: any): any{
-            const origin = {x:500, y:500}
-            const r = 400
-            const theta = -Math.PI
-            const member = 348
-            const membersPerRow = 12
-            const distanceBetweenMembers = 14
-            function round(i:number):number{
-                return Math.ceil(i/membersPerRow)*membersPerRow
-            }
-            return circles
-               .transition(2000)
-               .ease("linear")
-               .attr("cx", (p: Person, i: number) => {return origin.x+(r-(i%membersPerRow)*distanceBetweenMembers)*Math.cos(theta*(member-round(i))/member)})
-               .attr("cy", (p: Person, i: number) => {return origin.y+(r-(i%membersPerRow)*distanceBetweenMembers)*Math.sin(theta*(member-round(i))/member)}) 
-               .attr("r", 6)    
+
+    basePosition(circles: d3.Selection<Person>): void {
+        const origin = { x: 500, y: 500 }
+        const r = 400
+        const theta = -Math.PI
+        const member = 348
+        const membersPerRow = 12
+        const distanceBetweenMembers = 14
+        function round(i: number): number {
+            return Math.ceil(i / membersPerRow) * membersPerRow
+        }
+        circles
+            .transition(2000 + '')
+            .ease('linear')
+            .attr('cx', (p: Person, i: number) => origin.x + (r - (i % membersPerRow) * distanceBetweenMembers) * Math.cos(theta * (member - round(i)) / member))
+            .attr('cy', (p: Person, i: number) => origin.y + (r - (i % membersPerRow) * distanceBetweenMembers) * Math.sin(theta * (member - round(i)) / member))
+            .attr('r', 6)
     }
-    
-    componentDidUpdate(){
-        const {isFetching, people, dispatch} = this.props
-        
-        
-        let sortedByParties = _.chain(people).sortBy('party').value()
-        this.circles = this.svg.selectAll("circle")
+
+    componentDidUpdate() {
+        const {people, dispatch} = this.props
+
+        let sortedByParties = _.chain(people).sortBy(p => p.party).value()
+        this.circles = this.svg.selectAll('circle')
             .data(sortedByParties)
             .enter()
-            .append("circle")
-            .attr("r", 6)
+            .append('circle')
+            .attr('r', 6)
             .on('click', (p: Person) => {
                 dispatch(selectPerson(p))
             })
-            .attr("fill", (p: Person) => {
-                return partyColor(p.party)
+            .attr('fill', (p: Person) => {
+                return PartyColor[p.party]
             })
-            .attr("cx", 0)
-            .attr("cy", 0)
-        
-        
+            .attr('cx', 0)
+            .attr('cy', 0)
+
         this.basePosition(this.circles)
-        
-        
-       
     }
-    
+
     render(): JSX.Element {
-        
-        return <div id="d3root">
-            <button onClick={() => {this.circlePosition(this.circles)}}>circle</button>
-            <button onClick={() => {this.basePosition(this.circles)}}>base</button>
+
+        return <div>
+            <button onClick={() => { this.circlePosition(this.circles) } }>circle</button>
+            <button onClick={() => { this.basePosition(this.circles) } }>base</button>
+            <svg width={this.w} height={this.h} ref={this.svgRef}></svg>
             <PersonPanel/>
         </div>
     }
